d942e3602df1ba5914a94164270d38a9d84e1f62
Kaly-0 <flavia.fernandes@oteria.fr>
2026-09-07 06:54:15 -0400

    Final correction for real for real

--- asm01/asm01.s
+++ asm01/asm01.s
@@ -1,7 +1,7 @@
 global _start
 
 section .data
-    msg db "1337", 10
+    msg db "1337", 0x0A
 
 section .text
 _start:
@@ -9,7 +9,7 @@ _start:
     mov rax, 1
     mov rdi, 1
     mov rsi, msg
-    mov rdx, 6
+    mov rdx, 5
     syscall 
 
     mov rax, 60
