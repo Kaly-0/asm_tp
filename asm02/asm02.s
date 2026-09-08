2ceb6c3c8505e35388e06995e6a1e1389ef7887a
Flavia <flaviamariaaugustafernandes@gmail.com>
2026-09-08 06:12:49 -0400

    version finale

--- asm02/asm02.s
+++ asm02/asm02.s
@@ -4,7 +4,12 @@ section .data
     msg db 0x31, 0x33, 0x33, 0x37, 0x0A
 
 section .bss
-    input resb 256 "42", 0x0A
+    input resb 256
+
+_error:
+    mov rax, 60
+    mov rdi, 1
+    syscall
 
 section .text 
 _start:
@@ -14,8 +19,17 @@ _start:
     mov rdx, 256
     syscall
 
-    cmp 
-    
+    cmp byte [input], 0x34
+    jne _error
+
+    cmp byte [input +1], 0x32
+    jne _error
+
+    je msg
     mov rax, 60
     mov rdi, 0
+    syscall
+
+    mov rax, 60
+    mov rdi, 1
     syscall
